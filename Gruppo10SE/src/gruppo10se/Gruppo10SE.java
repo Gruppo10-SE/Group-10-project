ee9256262b5713f4d49f85ff115dcea721a2be37
giaco <giaco@DESKTOP-P2LPRMI.mshome.net>
2021-11-17 18:38:04 +0100

    by Giacomo

--- Gruppo10SE/src/gruppo10se/Gruppo10SE.java
+++ Gruppo10SE/src/gruppo10se/Gruppo10SE.java
@@ -17,6 +17,7 @@ public class Gruppo10SE {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello world by Stefano");
+        System.out.println("Hello worl by Giacomo");
     }
 
 }
