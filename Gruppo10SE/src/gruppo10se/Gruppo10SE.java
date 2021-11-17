f0895eb912c08820e292e0911bbbc50e43021775
phesmatos <s.lacorte1@studenti.unisa.it>
2021-11-17 18:52:20 +0100

    Greetings by Stefano 3

--- Gruppo10SE/src/gruppo10se/Gruppo10SE.java
+++ Gruppo10SE/src/gruppo10se/Gruppo10SE.java
@@ -16,7 +16,7 @@ public class Gruppo10SE {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello world by Stefano");
+        System.out.println("Hello world by Stefanoooo");
         System.out.println("Hello world by Giacomo");
         System.out.println("Hello world by Federicaa");
         System.out.println("Hello world by Gerry");
