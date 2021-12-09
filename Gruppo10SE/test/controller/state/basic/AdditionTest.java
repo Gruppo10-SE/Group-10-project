63d81b10f1fa0c79d3ea223fdc599fd91ca3dff4
phesmatos <s.lacorte1@studenti.unisa.it>
2021-12-09 19:13:03 +0100

    Merge state-pattern into main

---
+++ Gruppo10SE/test/controller/state/basic/AdditionTest.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller.state.basic;
+
+import gruppo10se.CartesianComplex;
+import gruppo10se.StackDataStructure;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author user
+ */
+public class AdditionTest {
+    
+    public AdditionTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of doBasicOperation method, of class Addition.
+     */
+    @Test
+    public void testDoBasicOperation() {
+        System.out.println("doBasicOperation");
+        StackDataStructure<CartesianComplex> complexStack = null;
+        Addition instance = new Addition();
+        int expResult = 0;
+        int result = instance.doBasicOperation(complexStack);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getMessage method, of class Addition.
+     */
+    @Test
+    public void testGetMessage() {
+        System.out.println("getMessage");
+        Addition instance = new Addition();
+        String expResult = "";
+        String result = instance.getMessage();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
